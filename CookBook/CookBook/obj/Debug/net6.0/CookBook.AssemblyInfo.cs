5438d6a4dd019707b929c95a0b889e0725ea486b
DanAleksiev <toksikar46@gmail.com>
2024-02-27 10:37:12 +0000

    Add and remove button implemented
    the foundation to send the ingredients to the controller are set

--- CookBook/CookBook/obj/Debug/net6.0/CookBook.AssemblyInfo.cs
+++ CookBook/CookBook/obj/Debug/net6.0/CookBook.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cd48555286d9e24ebb8a9b7d88e159cd5049b1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8eca592141f311a5d5547053e1d313294374c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
