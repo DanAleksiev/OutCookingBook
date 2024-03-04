da2fe512ad624e02c9e811d571139081abcaeb2d
DanAleksiev <toksikar46@gmail.com>
2024-03-04 23:20:13 +0000

    details ajusted a bit need work!

--- CookBook/CookBook/obj/Debug/net6.0/CookBook.AssemblyInfo.cs
+++ CookBook/CookBook/obj/Debug/net6.0/CookBook.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5a6f510a7d6fef124dd7c835f2d85af8c36fb8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07bb02155ecd7847d16540e83e71f7bc7aab2c67")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
