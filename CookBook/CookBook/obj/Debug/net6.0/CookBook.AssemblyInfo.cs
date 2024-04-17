c23327222eb9d3379c595f4ee3d8d27432cd2245
DanAleksiev <toksikar46@gmail.com>
2024-04-17 18:21:27 +0100

    buttons and visual changes

--- CookBook/CookBook/obj/Debug/net6.0/CookBook.AssemblyInfo.cs
+++ CookBook/CookBook/obj/Debug/net6.0/CookBook.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1c65a4ec2d94b92716e548962b54b87d27d68d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27166eef42b33fbb46c102fe53bf2532a6a8a6f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
